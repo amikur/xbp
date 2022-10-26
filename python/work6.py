c08754c41f22c7a1c130e469b6d35612f6b48578
amikur <r202201767zb@jindai.jp>
2022-10-27 02:21:50 +0900

    2022

--- python/work6.py
+++ python/work6.py
@@ -37,10 +37,12 @@ if 1<people<5:
     num = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
     patt=("♥","♣,","♤","♢")
     print("ディーラー:はじめに、私のカードを2枚引き、そのうち１枚をお見せします。")
-    print(random.sample(patt, k=1)) 
-    print(random.sample(num, k=1) )
+    deapat=random.sample(patt, k=1)
+    dea=random.sample(num, k=1)
+    print(deapat)
+    print(dea)
 
-
+# ---プレーヤー1セットここから---
     print("ディーラー：",name1,"さん、カードを2枚お配りします。",name2,"さんは、画面を見ないでください")
     choices=input("　　　　　　準備はよろしいですか？ yes/no:")
     if choices in ['y', 'ye', 'yes']:
@@ -53,85 +55,139 @@ if 1<people<5:
       print(b)
       print("です。")
       print("合計は、")
-      total=(sum(a+b))
-      print(total)
-      print("です。")
+      total1=(sum(a+b))
+      print(total1,"点です。")
       choices=input("ディーラー：カードを追加しますか？ yes/no:")
       # カードを追加する
       if choices in ['y', 'ye', 'yes']:
         print(random.choices(patt, k=1))
         c=random.sample(num, k=1) 
         print(c)
-        total=(sum(a+b+c))
+        total1=(sum(a+b+c))
         print("合計は、")
-        print(total)
-        print("です。")
+        print(total1,"点です。")
 
 
-        if total>21:    
+        if total1>21:    
           print("ディーラー：21を超えてしまいました。残念ながら負けです。")
           zzz=(input("次へ進みます。yを押してください"))
 
-        else:
-          zzz=(input("次へ進みます。yを押してください"))
+        elif total1<22:
+          choices=input("ディーラー：カードを追加しますか？ yes/no:")
+          # カードを追加する
+          if choices in ['y', 'ye', 'yes']:
+            print(random.choices(patt, k=1))
+            d=random.sample(num, k=1) 
+            print(d)
+            total1=(sum(a+b+c+d))
+            print("合計は、")
+            print(total1,"点です。")
 
-# カードを追加しない
-    else:
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print(".")
-      print("ディーラー：",name2,"さんの番です。",name1,"さんは画面を見ないでください。")
+
+          if total1>21:    
+            print("ディーラー：21を超えてしまいました。残念ながら負けです。")
+            zzz=(input("次へ進みます。yを押してください"))
+
+            choices=input("ディーラー：カードを追加しますか？ yes/no:")
+            # カードを追加する
+            if choices in ['y', 'ye', 'yes']:
+              print(random.choices(patt, k=1))
+              e=random.sample(num, k=1) 
+              print(e)
+              total1=(sum(a+b+c+d+e))
+              print("合計は、")
+              print(total1,"点です。")
+
+
+              if total1>21:    
+                print("ディーラー：21を超えてしまいました。残念ながら負けです。")
+                zzz=(input("次へ進みます。yを押してください"))
+
+              elif total1<22:
+                choices=input("ディーラー：カードを追加しますか？ yes/no:")
+          # カードを追加する
+              if choices in ['y', 'ye', 'yes']:
+                print(random.choices(patt, k=1))
+                c=random.sample(num, k=1) 
+                print(c)
+                total1=(sum(a+b+c))
+                print("合計は、")
+                print(total1,"点です。")
+                zzz=(input("次へ進みます。yを押してください"))
+                # ---ここまで---
+
+
+          
+
+            
+
+# pythonブチギレver.
+    elif choices in ['n', 'no', ]:
+      print("python:はよ準備せんかい")
+      cho=input("　　　準備ええか？ yes/no:")
+      if cho in ['y', 'ye', 'yes']:
+        print(random.choices(patt, k=1))
+        a=random.sample(num, k=1)
+        print(a)
+        print("と、")
+        print(random.choices(patt, k=1))
+        b=random.sample(num, k=1)
+        print(b)
+        print("だ。")
+        print("合計は、")
+        total=(sum(a+b))
+        print(total,"点だよ。はぁ、、")
+        choices=input("python：カード引く？どっちにすんの？ yes/no:")
+        # カードを追加する
+        if choices in ['y', 'ye', 'yes']:
+          print(random.choices(patt, k=1))
+          c=random.sample(num, k=1) 
+          print(c)
+          total=(sum(a+b+c))
+          print("合計は、")
+          print(total,"点だ。")
+
+
+          if total>21:    
+            print("python：あっ、、21超えたよww残念だったねｗ")
+            zzz=(input("次行こう！！！！早くy押して"))
+
+          choices=input("python：カード引く？どっちにすんの？ yes/no:")
+            # カードを追加する
+          if choices in ['y', 'ye', 'yes']:
+            print(random.choices(patt, k=1))
+            d=random.sample(num, k=1) 
+            print(d)
+            total=(sum(a+b+c+d))
+            print("合計は、")
+            print(total,"点だ。")
+
+
+            if total>21:    
+              print("python：あっ、、21超えたよww残念だったねｗ")
+              zzz=(input("次へ進みます。yを押してください"))
+
+            elif total<22:
+              choices=input("python：カード引く？どっちにすんの？ yes/no:")
+          # カードを追加する
+              if choices in ['y', 'ye', 'yes']:
+                print(random.choices(patt, k=1))
+                e=random.sample(num, k=1) 
+                print(c)
+                otal=(sum(a+b+c+d+e))
+                print("合計は、")
+                print(total,"点です。")
+
+
+              if total>21:    
+                print("python：あっ、、21超えたよww残念だったねｗ")
+                zzz=(input("次へ進みます。yを押してください"))
+      else:
+        sys.exit("python:お前となんかやってられっか！！！！強制終了します。")
+          
+
+
+
 
 
 
@@ -196,7 +252,136 @@ if 1<people<5:
     print("ディーラー：",name2,"さん、カードを2枚お配りします。",name1,"さんは、画面を見ないでください")
     choices=input("　　　　　　準備はよろしいですか？ yes/no:")
     if choices in ['y', 'ye', 'yes']:
-      print(random.sample(int, k=2) )
+      print(random.choices(patt, k=1))
+      a=random.sample(num, k=1)
+      print(a)
+      print("と、")
+      print(random.choices(patt, k=1))
+      b=random.sample(num, k=1)
+      print(b)
+      print("です。")
+      print("合計は、")
+      total2=(sum(a+b))
+      print(total2)
+      print("です。")
+      choices=input("ディーラー：カードを追加しますか？ yes/no:")
+      # カードを追加する
+      if choices in ['y', 'ye', 'yes']:
+        print(random.choices(patt, k=1))
+        c=random.sample(num, k=1) 
+        print(c)
+        total2=(sum(a+b+c))
+        print("合計は、")
+        print(total2,"点です。")
+
+
+        if total2>21:    
+          print("ディーラー：21を超えてしまいました。残念ながら負けです。")
+          zzz=(input("次へ進みます。yを押してください"))
+
+        else:
+          choices=input("ディーラー：カードを追加しますか？ yes/no:")
+          # カードを追加する
+          if choices in ['y', 'ye', 'yes']:
+            print(random.choices(patt, k=1))
+            d=random.sample(num, k=1) 
+            print(c)
+            total2=(sum(a+b+c+d))
+            print("合計は、")
+            print(total2,"点です。")
+
+
+          if total2>21:    
+            print("ディーラー：21を超えてしまいました。残念ながら負けです。")
+            zzz=(input("次へ進みます。yを押してください"))
+
+          
+    
+      
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print(".")
+      print("ディーラー：",name1,"さん、",name2,"さん、勝負の時間です。")
+      zzz=(input("準備はよろしいですか？yを押してください"))
+      dea2=random.sample(num, k=1)
+      deapat2=random.sample(patt, k=1)
+
+      print("ディーラー：私が最初に引いたカードは、",deapat,"の",dea,"です。そして、もう１枚のカードは",deapat2,"の",dea2,"です。")
+      totaldea=(sum(dea+dea2))
+      print("ディーラー：現在の私の合計は、",totaldea,"です。")
+      if totaldea<17:
+        dea3=random.sample(num, k=1)
+        deapat3=random.sample(patt, k=1)
+        print("ディーラー：合計が17点に達していない為新しくカードを引きます。",deapat3,"の",dea3,"です。")
+        totaldea2=(sum(dea+dea2+dea3))
+        if totaldea2>21:
+          print("ディーラー：おっと、21を超えてしまいましたね。私の負けです。")
+          
+
+        if 16<totaldea2<22:
+          print("ディーラー：準備は整いました。勝負です。")
+          if total1>21 and total2>21:
+            print("ディーラー：",name1,"さん、",name2,"さん、21をオーバーしている様ですね。今回のゲームは私の勝ちです。")
+
+
+        
+
+
+
+
+    
+
         
         
             
