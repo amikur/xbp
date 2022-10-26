784ca1c499aa682124e7e815490f124cc26c101c
amikur <r202201767zb@jindai.jp>
2022-10-26 16:57:00 +0900

    2-

--- python/main.py
+++ python/main.py
@@ -1,1 +1,21 @@
+import random
+num=('♥Ａ', '♥２', '♥３', '♥４', '♥５', '♥６', '♥７', '♥８', '♥９', '♥10', '♥Ｊ', '♥Ｑ', '♥Ｋ'\
+      ,'♣Ａ', '♣２', '♣３', '♣４', '♣５', '♣６', '♣７', '♣８', '♣９', '♣10', '♣Ｊ', '♣Ｑ', '♣Ｋ'\
+        ,'♤Ａ', '♤２', '♤３', '♤４', '♤５', '♤６', '♤７', '♤８', '♤９', '♤10', '♤Ｊ', '♤Ｑ', '♤Ｋ'\
+          ,'♢Ａ', '♢２', '♢３', '♢４', '♢５', '♢６', '♢７', '♢８', '♢９', '♢10', '♢Ｊ', '♢Ｑ', '♢Ｋ')
 
+print("ディーラー:はじめに、私のカードを2枚引き、そのうち１枚をお見せします。") 
+print(random.sample(num, k=1) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
+print(random.sample(num, k=2) )
