6d6c600d3671bba9562cbf6ba69b99c348b0113d
amikur <r202201767zb@jindai.jp>
2023-03-15 12:02:25 +0900

    new

--- python/sce.py
+++ python/sce.py
@@ -1,9 +1,50 @@
-import tkinter
+import tkinter as tk
 import calendar
 import datetime
 from tkinter import font
 
 import PySimpleGUI as sg
+
+
+
+class TodoList:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Todo List")
+
+        self.tasks = []
+
+        self.task_label = tk.Label(master, text="Task:")
+        self.task_label.pack()
+
+        self.task_entry = tk.Entry(master)
+        self.task_entry.pack()
+
+        self.add_button = tk.Button(master, text="Add Task", command=self.add_task)
+        self.add_button.pack()
+
+        self.task_list = tk.Listbox(master)
+        self.task_list.pack()
+
+        self.remove_button = tk.Button(master, text="Remove Task", command=self.remove_task)
+        self.remove_button.pack()
+
+    def add_task(self):
+        task = self.task_entry.get()
+        if task:
+            self.tasks.append(task)
+            self.task_list.insert(tk.END, task)
+            self.task_entry.delete(0, tk.END)
+
+    def remove_task(self):
+        selection = self.task_list.curselection()
+        if selection:
+            task = self.task_list.get(selection[0])
+            self.task_list.delete(selection[0])
+            self.tasks.remove(task)
+
+
+
 
 
 
