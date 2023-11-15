8900156bb9fbdba59aeebe18fb7007d02b0e846f
Your Name <amik.351612@gmail.com>
2023-11-15 13:44:49 +0900

    a

---
+++ process/webprocess/camera.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var video = document.getElementById('camera');
+
+  if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+          .then(function (stream) {
+              video.srcObject = stream;
+          })
+          .catch(function (error) {
+              console.log("カメラの起動に失敗しました: ", error);
+          });
+  } else {
+      alert("お使いのブラウザはカメラ機能をサポートしていません。");
+  }
+});
